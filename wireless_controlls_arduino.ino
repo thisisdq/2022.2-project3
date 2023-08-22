7d6570688093fe92eb80ddde40ab44ae597fa55d
DanhQuy <danhquy1101@gmail.com>
2023-08-22 18:59:28 +0700

    FINAL

--- wireless_controlls_arduino.ino
+++ wireless_controlls_arduino.ino
@@ -135,14 +135,17 @@ void toggleLED()
     digitalWrite(ledPin, !digitalRead(ledPin)); // Toggle LED state
 }
 
-void updateLEDStatus(){
-    if (WiFi.status() == WL_CONNECTED){
+void updateLEDStatus()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
         HTTPClient http;
         String url = "http://192.168.0.101:5000/getLEDStatus";
         http.begin(client, url);
 
         int httpResponseCode = http.GET();
-        if (httpResponseCode == HTTP_CODE_OK){
+        if (httpResponseCode == HTTP_CODE_OK)
+        {
             String response = http.getString();
 
             // Parse JSON response
@@ -150,11 +153,14 @@ void updateLEDStatus(){
             deserializeJson(jsonBuffer, response);
 
             const char *ledStatus = jsonBuffer["ledStatus"];
-            if (ledStatus){
-                if (strcmp(ledStatus, "ON") == 0){
+            if (ledStatus)
+            {
+                if (strcmp(ledStatus, "ON") == 0)
+                {
                     digitalWrite(ledPin, HIGH);
                 }
-                else if (strcmp(ledStatus, "OFF") == 0){
+                else if (strcmp(ledStatus, "OFF") == 0)
+                {
                     digitalWrite(ledPin, LOW);
                 }
             }
@@ -167,27 +173,34 @@ void loop()
 {
     float temperature = readTemperature();
     float humidity = readHumidity();
-    if (!isnan(temperature)) {
+    int inf = digitalRead(infraredPin);
+    if (!isnan(temperature))
+    {
         if (temperature != previousTemperature)
         {
             sendTemperature(temperature);
             previousTemperature = temperature;
         }
     }
-    if (!isnan(humidity)) {
+    if (!isnan(humidity))
+    {
         if (humidity != previousHumidity)
         {
             sendHumidity(humidity);
             previousHumidity = humidity;
         }
     }
-    int inf = digitalRead(infraredPin);
-    if (!isnan(inf)){
-        if (inf != previousInfrared){
-            if (inf == 0){
+
+    if (!isnan(inf))
+    {
+        if (inf != previousInfrared)
+        {
+            if (inf == 0)
+            {
                 sendInfrared("true");
             }
-            else{
+            else
+            {
                 sendInfrared("false");
             }
             previousInfrared = inf;
